0d32be5a476b4de6a862910e55cb4cb09c448fe4
Manon Oriol <oriol.manon@gmail.com>
2020-11-23 19:24:09 +0100

    added validations to models

--- app/models/support.rb
+++ app/models/support.rb
@@ -1,4 +1,6 @@
 class Support < ApplicationRecord
   belongs_to :user
   belongs_to :charity
+  has_many :users
+  has_many :charities
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :searches
+  has_many :supports, dependent: :destroy
+  has_many :charities, through: :supports
+  has_one_attached :photo
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
