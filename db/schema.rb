acb87d1dfcf5f69281a7fc2abd7726a61dd42ad3
antoine maurel <antoine.maurel@gmail.com>
2020-11-23 18:09:51 +0100

    creation des models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_165334) do
+ActiveRecord::Schema.define(version: 2020_11_23_170415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.bigint "charity_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["charity_id"], name: "index_articles_on_charity_id"
+  end
+
+  create_table "charities", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "address"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_searches_on_user_id"
+  end
+
+  create_table "supports", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "charity_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["charity_id"], name: "index_supports_on_charity_id"
+    t.index ["user_id"], name: "index_supports_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +58,14 @@ ActiveRecord::Schema.define(version: 2020_11_23_165334) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "charities"
+  add_foreign_key "searches", "users"
+  add_foreign_key "supports", "charities"
+  add_foreign_key "supports", "users"
 end
