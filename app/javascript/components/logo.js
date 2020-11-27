814c7c1d38855149a2f405fca2519e365b0f19a3
Tristan Simon <tristan.simon1@outlook.fr>
2020-11-27 11:43:00 +0100

    change home front

---
+++ app/javascript/components/logo.js
@@ -0,0 +1,8 @@
+document.querySelector("#.searchbar").addEventListener("mouseover", changeClassLogo);
+const logo = querySelector(".logo")
+
+ function changeClassLogo() {
+ 	setTimeout(function) {logo.classList.toggle("stick-logo"); 1000}	
+}
+
+export { changeClassLogo };
