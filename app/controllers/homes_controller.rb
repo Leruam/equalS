3f78fc2c8bbe79394e0719ab3e1cd43a5924d889
Manon Oriol <oriol.manon@gmail.com>
2020-11-30 19:38:00 +0100

    seed, user-profile, search page counter

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,5 +2,7 @@ class HomesController < ApplicationController
 
   def show
     skip_authorization # pour ne pas bloquer via pundit
+    @global_search_count = Search.count + 6_227_921
+    @global_money_count = @global_search_count * 0.05
   end
 end
