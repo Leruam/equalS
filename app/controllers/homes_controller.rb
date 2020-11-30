8f1c6b6b57b436e454d9c85c3472fff2f6e1d497
antoine maurel <antoine.maurel@gmail.com>
2020-11-30 18:13:41 +0100

    devise pundit et embryon de barre de recherche

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
 
   def show
     skip_authorization # pour ne pas bloquer via pundit
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,11 @@
 class SearchesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :create --> mieux d'utiliser simple_form qui gere le token ou de mettre un authenticity_token dans le form
+  skip_before_action :authenticate_user!, only: :create
 
   def create
-    new_search = Search.create(content: params[:q], user: current_user)
+    new_search = Search.create(content: params[:search][:q], user: current_user)
     authorize new_search # pundit
-    redirect_to search_path(q: params[:search][:q]) # il faut mettre une cle / valeur dans le path ou un objet
+    redirect_to search_path(q: params[:search][:q])
+    # il faut mettre une cle / valeur dans le path ou un objet
+    # params[:search] --> simple form range tout ce que renvoie le form dans une key correspondant au nom du model
   end
 end
