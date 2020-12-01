8662433f091272e8c3c18d7afb3fd3dc118e8214
antoine maurel <antoine.maurel@gmail.com>
2020-12-01 17:21:09 +0100

    fix consistency btw counters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
     @user_total_search_count = Search.where(user: @user).count + 1556
     @user_today_search_count = Search.where(user: @user, created_at: Date.today.beginning_of_day..Date.today.end_of_day).count + 8
     @user_charity_count = Charity.where(id: @user.charities.ids).count
-    @global_search_count = Search.count + 6_227_921
-    @global_search_count_today = Search.where(created_at: Date.today.beginning_of_day..Date.today.end_of_day).count + 8326
     @global_charity_count = Charity.count
+    @global_search_count = global_search
+    @global_search_count_today = global_search_today
   end
 end
