365da857669f99921c1b532b06276a76e43f748c
Manon Oriol <oriol.manon@gmail.com>
2020-12-03 12:45:17 +0100

    remove support button ok

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -4,5 +4,8 @@ class CharitiesController < ApplicationController
     skip_authorization
     @charity = Charity.find(params[:id])
     @articles = Article.where(charity_id: @charity.id)
+    @support = @charity.supports.find_by(user_id: current_user.id)
+    # .where  => [record] or [ ]
+    # .find_by => record or error, finds the first match (in 99% of cases used when there's only 1 match)
   end
 end
